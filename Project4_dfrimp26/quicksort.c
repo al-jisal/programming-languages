ef72706a57c9c70d8b398674477925abb632b53e
al_jisal <aljisal.frimpong@gmail.com>
2026-04-03 00:41:51 -0400

    added folders for P 4 and 5

---
+++ Project4_dfrimp26/quicksort.c
@@ -0,0 +1,33 @@
+/**
+ * Given an array of random integers, sort it in such a way that the even 
+ * numbers appear first and the odd numbers appear later. The even numbers 
+ * should be sorted in descending order and the odd numbers should be sorted 
+ * in ascending order.
+ *
+ * Ying Li
+ * 08/02/2016
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* the comparator funciton used in qsort */
+int comparator (const void *p, const void *q) {
+	// Your code here
+}
+
+int main (int argc, char **argv) {
+	int ary[] = {10, 11, 1, 8, 9, 0, 13, 4, 2, 7, 6, 3, 5, 12};
+	
+	int size = sizeof(ary) / sizeof(int);
+	
+	qsort((void *) ary, size, sizeof(int), comparator);
+	
+	printf("The sorted array is: ");
+	for (int i = 0; i < size; i++) {
+		printf("%d ", ary[i]);
+	}
+	printf("\n");
+	
+	return 0;
+}
