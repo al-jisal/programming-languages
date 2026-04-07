a794056f191fc1b1386293b8a2c338cd2664d369
al_jisal <aljisal.frimpong@gmail.com>
2026-04-07 15:09:12 -0400

    debugged it to pass test cases

--- Project5_dfrimp26/my_linkedlist.c
+++ Project5_dfrimp26/my_linkedlist.c
@@ -14,17 +14,8 @@ LinkedList * ll_create() {
     LinkedList * ll = ( LinkedList * ) malloc(sizeof(LinkedList));
     if ( ll == NULL ) return NULL;
 
-    ll->head = ( Node * ) malloc( sizeof( Node ) );
-    if ( ll->head == NULL ) {
-        free( ll );
-        return NULL;
-    }
-
-    ll->tail = ( Node * ) malloc( sizeof( Node ) );
-    if ( ll->tail == NULL ) {
-        free( ll );
-        return NULL;
-    }
+    ll->head = NULL;
+    ll->tail = NULL;
 
     return ll;
 };
@@ -34,22 +25,35 @@ void ll_push(LinkedList *l, void *data) {
 
     Node * node = ( Node * ) malloc( sizeof( Node ) );
     if ( node == NULL ) return;
-
     node->data = data;
     node->next = l->head;
-    l->head->prev = node;
+    node->prev = NULL;
+
+    if( l->head != NULL ){
+        l->head->prev = node;
+    } else{
+        l->tail = node;
+    }
 
     l->head = node;
 };
 
 void *ll_pop(LinkedList *l) {
-    if ( l == NULL ) return NULL;
+    if ( l == NULL || l->head == NULL) return NULL;
 
     Node * node = l->head;
-    l->head = node->next;
-    l->head->prev = NULL;
+    void * data = node->data;
 
-    return node->data;
+    l->head = node->next;
+
+    if( l->head != NULL ) {
+        l->head->prev = NULL;
+    } else{
+        l->tail = NULL;
+    }
+
+    free( node );
+    return data;
 };
 
 void ll_append(LinkedList *l, void *data) {
@@ -57,16 +61,21 @@ void ll_append(LinkedList *l, void *data) {
 
     Node * node = (Node *) malloc( sizeof( Node ) );
     if( node == NULL ) return;
-    
     node->data = data;
+    node->next = NULL;
     node->prev = l->tail;
 
-    l->tail->next = node;
+    if( l->tail != NULL ) {
+        l->tail->next = node;
+    } else{
+        l->head = node;
+    }
+
     l->tail = node;
 };
 
 void ll_map(LinkedList *l, void (*mapfunc)(void *)) {
-    if( l == NULL ) return;
+    if( l == NULL || mapfunc == NULL ) return;
 
     Node * node = l->head;
     while( node != NULL) {
